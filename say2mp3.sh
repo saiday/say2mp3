c36ad143ac507dd9cdcd0dae07c601ec01b1cd8d
Saiday <feocms@gmail.com>
2015-09-13 20:29:23 +0800

    first commit

---
+++ say2mp3.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+file="temp.txt"
+if [ ! -f "$file" ] ; then
+  touch "$file"
+fi
+echo $1 >> "$file"
+
+aiffm="a.aiff"
+mp3m="$(basename "$1" ".txt").mp3"
+
+say -f "$file" -o "$aiffm"
+lame --quiet -m m "$aiffm" "$mp3m"
+rm -rf "$aiffm"
+rm -rf "$file"
+
+echo "Writed" $mp3m
